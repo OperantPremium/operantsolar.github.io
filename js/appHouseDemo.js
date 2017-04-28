9c91146aafdb2e849bd0eae894336995f72cbaa1
bumbledad <randy.king@operantsolar.com>
2017-04-27 21:12:12 -0700

    finally works

--- js/appHouseDemo.js
+++ js/appHouseDemo.js
@@ -252,7 +252,6 @@ var fleetLink = {
           type: 'POST',
           success : function(response) {
             //console.log("response: " + response);
-            new Audio("img/smallBell2.wav").play();  // sound chime to indicate successful data packet reception
             displayData(response);
             setTimeout(function(){document.getElementById("expressInterestPacket").disabled = false;}, 14000 );
           },
@@ -262,7 +261,7 @@ var fleetLink = {
             setTimeout(function(){document.getElementById("expressInterestPacket").disabled = false;}, 10000 );
           }
         });
-                    new Audio("img/smallBell2.wav").play();  // sound chime to indicate successful data packet reception
+        new Audio("img/smallBell2.wav").play();  // sound chime to indicate successful data packet reception
       }
 
       // Display the result fetched after sending the modbus command.
