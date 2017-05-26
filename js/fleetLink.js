4998b9bc59f9cc3694e9604a94962d75622b3f4d
bumbledad <randy.king@operantsolar.com>
2017-05-26 15:26:20 -0700

    Changes to incorporate better format with full function of WiFI Scan

--- js/fleetLink.js
+++ js/fleetLink.js
@@ -15,26 +15,47 @@ var interest = {
     'url' : "https://agent.electricimp.com/QGO7JQAzyiev"
 }
 
+
+// Agent URLs as reference for GATEWAY
+//SN402 = /oHMQMg_lcxsT
+//SN403 = /wXqOLIl3KiLB
+//SN404 = /QGO7JQAzyiev
+//SN405 = /CyPoe3l9E5Od
+//SN406 = /hxsSiYETEEpd
+//SN407 = /VifAbahCX8ux
+//SN506 (Blue) = /oGQ_PBSAUppO
+//SN508 (Orange) = /2866vQYBgUpC
+//SN509 (Green) = /D1PRYwJmmHAi
+//SN513 (Black) = /ZT8GBL-7RrgD
+//SN514 (Yellow) = /609atPXTxkX7
+//SN515 = /VRa-gimZfDGJ
+
 // Choose gateway unit
 function setGateway(requestedGateway) {
     gateway = requestedGateway;
     switch(requestedGateway) {
+         case "SN402":
+            interest.url = "https://agent.electricimp.com/oHMQMg_lcxsT";
+            break;
         case "SN403":
             interest.url = "https://agent.electricimp.com/wXqOLIl3KiLB";
-            console.log("Setting gateway to SN403");
             break;
         case "SN404":
             interest.url = "https://agent.electricimp.com/QGO7JQAzyiev";
-            console.log("Setting gateway to SN404");
+            break;            
+        case "SN405":
+            interest.url = "https://agent.electricimp.com/CyPoe3l9E5Od";
             break;
         case "SN406":
             interest.url = "https://agent.electricimp.com/hxsSiYETEEpd";
-            console.log("Setting gateway to SN406");
+            break;
+        case "SN407":
+            interest.url = "https://agent.electricimp.com/hxsSiYETEEpd";
             break;            
         default:
-            interest.url = "https://agent.electricimp.com/QGO7JQAzyiev";
-            console.log("Setting gateway to default");
+            interest.url = "https://agent.electricimp.com/VifAbahCX8ux";
     }
+    console.log("Setting gateway to " + requestedGateway);
   }
 
 
@@ -49,27 +70,48 @@ function chooseNetwork(){
     interest.parameters = wiFiSSID;
 }
 
+// Device ID hashes as reference for TARGET
+//SN402 = D85F6461EB91
+//SN403 = 018C268ECB5B
+//SN404 = 2BF6EF3EFD90
+//SN405 = 718A34D8423A
+//SN406 = C5F6371C8A03
+//SN407 = 4CA33E88EDAA
+//SN506 (Blue) = C3B996B9F76C
+//SN508 (Orange) = 730D72A6E22F
+//SN509 (Green) = 16240A06C1FC
+//SN513 (Black) = DF04146F1DF0
+//SN514 (Yellow) = 00E329B56259
+//SN515 = 4E44238D7110
 
 // Choose target unit
 function setTarget(requestedTarget) {
     target = requestedTarget;
     switch(requestedTarget) {
+        case "SN402":
+            interest.deviceIdHash = "D85F6461EB91";
+             break;
         case "SN403":
             interest.deviceIdHash = "018C268ECB5B";
-            console.log("Setting target to SN403");
             break;
         case "SN404":
             interest.deviceIdHash = "2BF6EF3EFD90";
-            console.log("Setting target to SN404");
+            break;
+        case "SN405":
+            interest.deviceIdHash = "718A34D8423A";
             break;
         case "SN406":
             interest.deviceIdHash = "C5F6371C8A03";
-            console.log("Setting target to SN406");
+            break;
+        case "SN407":
+            interest.deviceIdHash = "4CA33E88EDAA";
             break;
         default:
             interest.deviceIdHash = "2BF6EF3EFD90";
             console.log("Setting target to default");
     }
+    console.log("Setting target to " + requestedTarget); 
+
 }
 
 
@@ -79,12 +121,9 @@ function setTarget(requestedTarget) {
         //interest.parameters = wiFiSSID;
         console.log(interest);
 
-        var waitDisplay = "Scanning WiFi of " + target;
+        var waitDisplay = "Scanning " + target;
         if (target != gateway){
-            waitDisplay += " (via " + gateway + ")";
-        }
-        else {
-            waitDisplay += " (direct)"
+            waitDisplay += " via " + gateway ;
         }
 
         // Disable button during request
@@ -100,23 +139,16 @@ function setTarget(requestedTarget) {
               success : function(response) {
                     var successDisplay = target + ": " + response;
                     if (target != gateway){
-                        successDisplay += " (via " + gateway + ")";
-                    }
-                    else {
-                    successDisplay += " (direct)"
+                        successDisplay += " via " + gateway;
                     }
                     console.log(successDisplay);
                     buttonID.innerHTML = successDisplay;
-                    // Enable button after response
-                    //buttonID.disabled = false;
                     buttonID.style.background='#90A878';
               },
               error : function(jqXHR, textStatus, err) {
                     var errorResponse = jqXHR.status + ' ' + textStatus + ': ' + err + ' - ' + jqXHR.responseText;
                     console.log(errorResponse);
                     buttonID.innerHTML = errorResponse;
-                    // Enable button after response
-                    //buttonID.disabled = false;
                     buttonID.style.background='#90A878';
               }
         });
