06ed30d68f10c4ddceced6875b4be379f6d1bbaa
bumbledad <randy.king@operantsolar.com>
2017-06-12 15:43:17 -0700

    Pushing new multiple network units for QA

--- js/fleetLink.js
+++ js/fleetLink.js
@@ -16,24 +16,57 @@ var interest = {
 }
 
 
+// Device ID hashes as reference for TARGET
 // Agent URLs as reference for GATEWAY
-//SN402 = /oHMQMg_lcxsT
-//SN403 = /wXqOLIl3KiLB
-//SN404 = /QGO7JQAzyiev
-//SN405 = /CyPoe3l9E5Od
-//SN406 = /hxsSiYETEEpd
-//SN407 = /VifAbahCX8ux
-//SN506 (Blue) = /oGQ_PBSAUppO
-//SN508 (Orange) = /2866vQYBgUpC
-//SN509 (Green) = /D1PRYwJmmHAi
-//SN513 (Black) = /ZT8GBL-7RrgD
-//SN514 (Yellow) = /609atPXTxkX7
-//SN515 = /VRa-gimZfDGJ
+//----------------------------------------------------
+
+
+// R&D development units - 918.5 MHz
+//----------------------------------------------------
+//SN402                  D85F6461EB91    /oHMQMg_lcxsT
+//SN403                  018C268ECB5B    /wXqOLIl3KiLB
+//SN404                  2BF6EF3EFD90    /QGO7JQAzyiev
+//SN405                  718A34D8423A    /CyPoe3l9E5Od
+//SN406                  C5F6371C8A03    /hxsSiYETEEpd
+//SN407                  4CA33E88EDAA    /VifAbahCX8ux
+
+// Customer demo houses - 917 MHz
+//----------------------------------------------------
+//SN503  No House        4E562573DBA0    /tRNE2WbS2CGw
+//SN506  Blue House      C3B996B9F76C    /oGQ_PBSAUppO
+//SN508  Orange House    730D72A6E22F    /2866vQYBgUpC
+//SN513  Black House     DF04146F1DF0    /ZT8GBL-7RrgD
+//SN514  Yellow House    00E329B56259    /609atPXTxkX7
+
+// Solmetric - 917 MHz
+//----------------------------------------------------
+//SN506                   C3B996B9F76C    /oGQ_PBSAUppO
+//SN511                   C1B16ADC8E57    /4R2NSeUUtys8
+//SN513                   DF04146F1DF0    /ZT8GBL-7RrgD
+//SN514                   00E329B56259    /609atPXTxkX7
+
+// Field demo - 915.5 MHz
+//----------------------------------------------------
+//SN505  Sugiyama        BA48D077C2A8    /RVKEMRdCLmKj
+//SN507  Gibson          67AE0AAFD4E2    /S6QExe1f2KTi
+//SN509  Green House     16240A06C1FC    /D1PRYwJmmHAi
+//SN512  Beckman         6917511534FD    /kRQMPFuKmzDM
+
+// Manufacturing - 915.5 MHz
+//----------------------------------------------------
+//SN501
+//SN502
+//SN504                  B930FA057CB6    /w8Bdk3n0iWt3
+//SN510 
+//SN511
+//SN515  Bad LoRa        4E44238D7110    /VRa-gimZfDGJ
+
 
 // Choose gateway unit
 function setGateway(requestedGateway) {
     gateway = requestedGateway;
     switch(requestedGateway) {
+// PROTO 4'S
          case "SN402":
             interest.url = "https://agent.electricimp.com/oHMQMg_lcxsT";
             break;
@@ -51,9 +84,55 @@ function setGateway(requestedGateway) {
             break;
         case "SN407":
             interest.url = "https://agent.electricimp.com/hxsSiYETEEpd";
-            break;            
+            break;      
+// PROTO 5'S
+         case "SN501":
+            interest.url = "";
+            break;      
+         case "SN502":
+            interest.url = "";
+            break; 
+         case "SN503":
+            interest.url = "https://agent.electricimp.com/tRNE2WbS2CGw";
+            break;      
+         case "SN504":
+            interest.url = "https://agent.electricimp.com/w8Bdk3n0iWt3";
+            break;
+         case "SN505":
+            interest.url = "https://agent.electricimp.com/RVKEMRdCLmKj";
+            break;      
+         case "SN506":
+            interest.url = "https://agent.electricimp.com/oGQ_PBSAUppO";
+            break; 
+         case "SN507":
+            interest.url = "https://agent.electricimp.com/S6QExe1f2KTi";
+            break;      
+         case "SN508":
+            interest.url = "https://agent.electricimp.com/2866vQYBgUpC";
+            break;  
+         case "SN509":
+            interest.url = "https://agent.electricimp.com/D1PRYwJmmHAi";
+            break;      
+         case "SN510":
+            interest.url = "";
+            break; 
+         case "SN511":
+            interest.url = "";
+            break;      
+         case "SN512":
+            interest.url = "https://agent.electricimp.com/kRQMPFuKmzDM";
+            break;
+         case "SN513":
+            interest.url = "https://agent.electricimp.com/ZT8GBL-7RrgD";
+            break;      
+         case "SN514":
+            interest.url = "https://agent.electricimp.com/609atPXTxkX7";
+            break; 
+         case "SN515":
+            interest.url = "https://agent.electricimp.com/VRa-gimZfDGJ";
+            break;      
         default:
-            interest.url = "https://agent.electricimp.com/VifAbahCX8ux";
+            interest.url = "";
     }
     console.log("Setting gateway to " + requestedGateway);
   }
@@ -70,24 +149,12 @@ function chooseNetwork(){
     interest.parameters = wiFiSSID;
 }
 
-// Device ID hashes as reference for TARGET
-//SN402 = D85F6461EB91
-//SN403 = 018C268ECB5B
-//SN404 = 2BF6EF3EFD90
-//SN405 = 718A34D8423A
-//SN406 = C5F6371C8A03
-//SN407 = 4CA33E88EDAA
-//SN506 (Blue) = C3B996B9F76C
-//SN508 (Orange) = 730D72A6E22F
-//SN509 (Green) = 16240A06C1FC
-//SN513 (Black) = DF04146F1DF0
-//SN514 (Yellow) = 00E329B56259
-//SN515 = 4E44238D7110
 
 // Choose target unit
 function setTarget(requestedTarget) {
     target = requestedTarget;
     switch(requestedTarget) {
+// PROTO 4'S
         case "SN402":
             interest.deviceIdHash = "D85F6461EB91";
              break;
@@ -106,8 +173,54 @@ function setTarget(requestedTarget) {
         case "SN407":
             interest.deviceIdHash = "4CA33E88EDAA";
             break;
+// PROTO 5'S
+        case "SN501":
+            interest.deviceIdHash = "";
+             break;
+        case "SN502":
+            interest.deviceIdHash = "";
+            break;
+        case "SN503":
+            interest.deviceIdHash = "4E562573DBA0";
+            break;
+        case "SN504":
+            interest.deviceIdHash = "B930FA057CB6";
+            break;
+        case "SN505":
+            interest.deviceIdHash = "BA48D077C2A8";
+            break;
+        case "SN506":
+            interest.deviceIdHash = "C3B996B9F76C";
+            break;
+        case "SN507":
+            interest.deviceIdHash = "67AE0AAFD4E2";
+            break;
+        case "SN508":
+            interest.deviceIdHash = "730D72A6E22F";
+            break;
+        case "SN509":
+            interest.deviceIdHash = "16240A06C1FC";
+            break;
+        case "SN510":
+            interest.deviceIdHash = "";
+            break;
+        case "SN511":
+            interest.deviceIdHash = "C1B16ADC8E57";
+            break;
+        case "SN512":
+            interest.deviceIdHash = "6917511534FD";
+            break;
+        case "SN513":
+            interest.deviceIdHash = "DF04146F1DF0";
+            break;
+        case "SN514":
+            interest.deviceIdHash = "00E329B56259";
+            break;
+        case "SN515":
+            interest.deviceIdHash = "4E44238D7110";
+            break;
         default:
-            interest.deviceIdHash = "2BF6EF3EFD90";
+            interest.deviceIdHash = "";
             console.log("Setting target to default");
     }
     console.log("Setting target to " + requestedTarget); 
